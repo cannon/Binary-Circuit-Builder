afc55279a63c3c0a59b97ee2e396c7e38721e4ec
bstrobel <bstrobel@d-69-91-176-37.dhcp4.washington.edu>
2015-10-18 02:41:03 -0700

    Added getInputs() since all gates should have an input.

--- core/src/com/github/jacksonc/circuitbuilder/Gate.java
+++ core/src/com/github/jacksonc/circuitbuilder/Gate.java
@@ -1,7 +1,10 @@
 package com.github.jacksonc.circuitbuilder;
 
+import java.util.List;
+
 public interface Gate {
 	public boolean getOutput();
 	public void think();
 	public void talk();
+	public List<Gate> getInputs();
 }
