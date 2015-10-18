2702338e0803715baa0ed809db1351d2d453ba38
Anthony Ha <anthonyha96@gmail.com>
2015-10-18 02:05:00 -0700

    Add basic rendering class code

--- core/src/com/github/jacksonc/circuitbuilder/CircuitBuilder.java
+++ core/src/com/github/jacksonc/circuitbuilder/CircuitBuilder.java
@@ -3,25 +3,41 @@ package com.github.jacksonc.circuitbuilder;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 
 public class CircuitBuilder extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private ShapeRenderer shape;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		shape = new ShapeRenderer();
+		shape.setAutoShapeType(true);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
+		shape.setProjectionMatrix(camera.combined);
+		shape.begin();
+		shape.line(new Vector2(0.f, 0.f), new Vector2(100.f, 100.f));
+		shape.end();
 	}
+	
+	public void setCamera(float x, float y, float scale) {
+		camera.position.x = x;
+		camera.position.y = y;
+		camera.zoom = scale;
+		camera.update();
+	}
+
 }
