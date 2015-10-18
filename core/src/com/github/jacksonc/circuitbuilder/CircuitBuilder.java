7077b495705ec2f809f7dcc44f39d26a5eaab79a
Anthony Ha <anthonyha96@gmail.com>
2015-10-18 08:38:55 -0700

    Implement gate rendering and camera resizing

--- core/src/com/github/jacksonc/circuitbuilder/CircuitBuilder.java
+++ core/src/com/github/jacksonc/circuitbuilder/CircuitBuilder.java
@@ -2,11 +2,14 @@ package com.github.jacksonc.circuitbuilder;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
 
 public class CircuitBuilder extends ApplicationAdapter {
@@ -16,6 +19,8 @@ public class CircuitBuilder extends ApplicationAdapter {
 	private SpriteBatch sprite;
 	private GridRenderer renderer;
 	private InputHandler inputHandler;
+	private Manager manager;
+	private BitmapFont font;
 	
 	@Override
 	public void create () {
@@ -25,24 +30,58 @@ public class CircuitBuilder extends ApplicationAdapter {
 		sprite = new SpriteBatch();
 		inputHandler = new InputHandler(this);
 		Gdx.input.setInputProcessor(inputHandler);
-		shape.setAutoShapeType(true);
 		renderer = new GridRenderer(sprite);
+		
+		manager = new Manager();
+		
+		font = new BitmapFont();
+		font.setColor(Color.BLACK);
 	}
 
 	@Override
 	public void render () {
 		inputHandler.update();
+		manager.update(Gdx.graphics.getDeltaTime());
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		sprite.setProjectionMatrix(camera.combined);
 		sprite.begin();
 		renderer.render(camera.position.x, camera.position.y, camera.zoom);
 		sprite.end();
-		shape.setProjectionMatrix(camera.combined);
-		shape.begin();
-		shape.line(new Vector2(0.f, 0.f), new Vector2(100.f, 100.f));
 		
+		shape.setProjectionMatrix(camera.combined);
+		shape.begin(ShapeType.Filled);
+		for (Gate g : manager.getGates()) {
+			DrawInfo info = g.getDrawInfo();
+			shape.box(info.x*32, info.y*32, 0, info.width*32, info.height*32, 0);
+			for (Gate input : g.getInputs()) { 
+				DrawInfo inputsInfo = input.getDrawInfo();
+				int correctedX = inputsInfo.x + inputsInfo.width;
+				
+				if (input.getOutput()) {
+					shape.setColor(Color.BLUE);
+				}
+				
+				shape.rectLine(correctedX * 32, inputsInfo.y * 32 + 16,
+						info.x * 32, info.y * 32 + 16, 4);
+				
+				shape.setColor(Color.WHITE);
+			}
+		}
 		shape.end();
+		
+		sprite.begin();
+		for (Gate g : manager.getGates()) {
+			DrawInfo info = g.getDrawInfo();
+			font.draw(sprite, info.name, info.x*32 + 8, info.y*32 + 21);
+		}
+		sprite.end();
+	}
+	
+	@Override
+	public void resize(int width, int height) {
+		camera.setToOrtho(false, width, height);
 	}
 	
 	public void setCamera(float x, float y, float scale) {
